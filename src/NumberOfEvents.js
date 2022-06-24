32348796ea203d318fdac839990e41ae20d427da
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-06-24 13:31:41 -0400

    added event count tests

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      numberOfEvents: 32,
+    };
+  }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ numberOfEvents: value });
+  };
+
+  render() {
+    return (
+      <div className="numberOfEvents">
+        <input
+          className="event-input"
+          type="number"
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChanged}
+        ></input>
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,6 +4,7 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents'
 
 describe('<App/> component', () => {
   let AppWrapper;
@@ -18,4 +19,8 @@ describe('<App/> component', () => {
   test('render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
+
+  test( 'render number of events', () =>{
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+  })
 });
