450aa8a2c942295ae1c0c4e2da8935cf5032ee61
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-07-21 11:52:41 -0400

    privacy path fix

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -30,7 +30,7 @@ o.svg"
         </div>
       </div>
       <a
-        href="https://bmcgaughan.github.io/meet/privacy.html"
+        href="https://bmcgaughan.github.io/meet-app/privacy.html"
         rel="nofollow noopener"
       >
         Privacy policy
